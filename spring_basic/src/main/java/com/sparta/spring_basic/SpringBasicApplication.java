c2b69718aab8100cc2abd66ebf8d4b297378f9b2
Parkjaeyong <pjjy92@gmail.com>
2023-02-04 19:08:18 +0900

    Add :  스프링 입문 개인과제 프로젝트 생성

---
+++ spring_basic/src/main/java/com/sparta/spring_basic/SpringBasicApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicApplication.class, args);
+    }
+
+}
---
+++ spring_basic/src/test/java/com/sparta/spring_basic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
