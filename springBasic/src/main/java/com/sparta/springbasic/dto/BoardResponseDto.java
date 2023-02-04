80a37881c6b2193d0ee09ad19d27fbe37c3fdd3c
Parkjaeyong <pjjy92@gmail.com>
2023-02-04 23:26:58 +0900

    fix : 게시글 등록 시 등록시간,생성시간 항목 추가

--- springBasic/src/main/java/com/sparta/springbasic/dto/BoardResponseDto.java
+++ springBasic/src/main/java/com/sparta/springbasic/dto/BoardResponseDto.java
@@ -5,15 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class BoardResponseDto {
     private String title;
     private String writer;
     private String content;
+    private LocalDateTime modifiedAt;
+    private LocalDateTime createdAt;
 
     public BoardResponseDto(Board board){
         this.title = board.getTitle();
         this.writer = board.getWriter();
         this.content = board.getContent();
+        this.modifiedAt = board.getModifiedAt();
+        this.createdAt = board.getCreatedAt();
     }
 }
