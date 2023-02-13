04e46a782c0e6ca07ce4a380f5d13de2a3a124d5
Parkjaeyong <pjjy92@gmail.com>
2023-02-14 00:20:37 +0900

    fix : 회원가입 비밀번호패턴, 권한 정보 수정 - closed #19

--- springBasic/src/main/java/com/sparta/springbasic/dto/SignupRequestDto.java
+++ springBasic/src/main/java/com/sparta/springbasic/dto/SignupRequestDto.java
@@ -14,7 +14,7 @@ public class SignupRequestDto {
     private String username;
 
     @Size(min = 8,max = 15, message = "패스워드는 8~15자리만 가능합니다")
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$",message = "패스워드는 대/소문자, 숫자만 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9\\\\d`~!@#$%^&*()-_=+]{8,15}$",message = "패스워드는 대/소문자, 숫자만 가능합니다.")
     private String password;
 
     private boolean adminCheck;
